243e31fcd3daad5883395a416ef1c7b5e0531a51
Raphael Zimeray <raphaelzimeray@me.com>
2024-01-13 16:05:27 +0100

    doneforfetch

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
-  pianos = [{brand: "Roland", model: "RD88", price: "1100 €"}, {brand: "Roland", model: "RD2000", price: "2100 €"}];
+export class HomepageComponent implements OnInit {
+  pianos: any = [];
+  ngOnInit() {
+    fetch("http://localhost:3000/pianos").then(response=> {
+      return response.json()
+    })
+    .then(response=>{
+      this.pianos = response;
+    })
+  }
 }
-
