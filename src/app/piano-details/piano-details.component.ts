a62971e9e0ec3be7bceceb2431b2d3e60aa24404
Raphael Zimeray <raphaelzimeray@me.com>
2024-01-14 15:56:16 +0100

    done for front

--- src/app/piano-details/piano-details.component.ts
+++ src/app/piano-details/piano-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-piano-details',
   templateUrl: './piano-details.component.html',
   styleUrls: ['./piano-details.component.css']
 })
-export class PianoDetailsComponent {
+export class PianoDetailsComponent implements OnInit {
 
+  piano:any = {};
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(){
+    const pianoId = this.route.snapshot.paramMap.get('id')
+    console.log(pianoId);
+    fetch(`http://localhost:3000/pianos/${pianoId}`).then(response=> {
+      return response.json()
+    })
+    .then(response=>{
+      this.piano = response;
+      console.log(this.piano);
+    })
+  }
 }
