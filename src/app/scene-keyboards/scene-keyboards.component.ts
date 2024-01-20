497ef80937a4567afd1c438681fb223045216a1e
Raphael Zimeray <raphaelzimeray@me.com>
2024-01-20 15:39:48 +0100

    done

--- src/app/scene-keyboards/scene-keyboards.component.ts
+++ src/app/scene-keyboards/scene-keyboards.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-scene-keyboards',
   templateUrl: './scene-keyboards.component.html',
   styleUrls: ['./scene-keyboards.component.css']
 })
-export class SceneKeyboardsComponent {
+export class SceneKeyboardsComponent implements OnInit {
+  scenekeyboards: any = [];
+  ngOnInit() {
+    fetch("http://localhost:3000/scene-keyboards").then(response =>{
+      return response.json()
+    })
+    .then(response=>{
+      this.scenekeyboards = response;
+      console.log(this.scenekeyboards);
+    })
+  }
 
 }
